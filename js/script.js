35bb9fe6032f0e269abff1193ab2470bc2963351
imneverdied <imneverdied@gmail.com>
2021-02-10 00:26:20 +0800

    1 file modified

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player("player", {
-    height: "90%",
+    height: "95%",
     width: "100%",
     videoId: "ampmDPg4QrM",
     events: {
